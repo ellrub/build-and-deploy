34318ba338884b61a2bb4805a6fccdf218566bd5
Ruben Ellefsen <ellefsen88@gmail.com>
2023-10-31 19:04:39 +0100

    add always show two digits on h, m, s

--- code/src/clock.js
+++ code/src/clock.js
@@ -1,7 +1,7 @@
 export const getTimeString = (now) => {
-    const hour = now.getHours();
-    const minute = now.getMinutes();
-    const seconds = now.getSeconds();
+    const hour = now.getHours().toString().padStart(2, "0");
+    const minute = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
     return `${hour}:${minute}:${seconds}`
 }
 
@@ -23,7 +23,6 @@ export const getMinutesLeftOfYear = (now) => {
     const startOfNextYearMillis = startOfNextYear.getTime();
     const nowMillis = now.getTime();
     const minutes =  (((startOfNextYearMillis - nowMillis) / 1000) / 60); 
-    // format to two decimals
     const formatted = (minutes).toFixed(2).split('.');
     return formatted
 }
